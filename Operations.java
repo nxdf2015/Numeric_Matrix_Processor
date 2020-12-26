6faa986ab3e6c3293ebe71998935fc83260dc16f
xavier niquet <niquet_xavier2002@yahoo.fr>
2020-12-26 15:36:49 +0100

    stage 3

--- Operations.java
+++ Operations.java
@@ -3,4 +3,5 @@ package processor;
 public interface Operations {
     public Matrix add(Matrix m);
     public Matrix multiply(int a);
+    public Matrix multiply(Matrix m) throws  IllegalArgumentException;
 }
